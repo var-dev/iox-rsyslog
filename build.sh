a1603bb190a2e16ca7e5ecbeef7a45e9b6bab6d5
var-dev <73451205+var-dev@users.noreply.github.com>
2020-10-25 23:19:02 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 rm -f package.tar && \
-git clone --branch=master https://github.com/var-dev/iox-rsyslog.git \
 ioxclient docker package var-dev/iox-rsyslog . && \
 ioxclient application install rsyslog package.tar && \
 ioxclient application activate  --payload activation.json --debug on rsyslog 
